91fc6f07371dc3684feb56b4cc203ee03cec6b46
eric valcik <eric.valcik@gmail.com>
2023-12-08 11:41:14 +0100

    day8 optimalizations +

--- src/day8/mod.rs
+++ src/day8/mod.rs
@@ -14,7 +14,7 @@ pub fn walk_network() -> usize {
             println!("step: {}, current: {:?}", steps.index, network.current);
         }
         network.walk(&mut steps);
-        if network.current.iter().all(|node| node.ends_with('Z')) {
+        if steps.get_index() == 0 && network.current.iter().all(|node| network.nodes[*node].from.ends_with('Z')) {
             break;
         }
     }
@@ -48,11 +48,18 @@ impl Steps {
 #[derive(Debug)]
 struct Network {
     nodes: Vec<Node>,
-    current: Vec<String>,
+    current: Vec<usize>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Node {
+    from: String,
+    left: usize,
+    right: usize,
+}
+
+#[derive(Debug, Clone)]
+struct NodeBuilder {
     from: String,
     left: String,
     right: String,
@@ -60,20 +67,31 @@ struct Node {
 
 impl Network {
     fn new(lines: Lines) -> Self {
-        let mut nodes = Vec::new();
+        let mut nodebuilders = Vec::new();
         for line in lines {
             let mut parts = line.trim().split(" = ");
             let from = parts.next().unwrap().trim().to_string();
             let mut sides = parts.next().unwrap().trim().split(", ");
             let left = sides.next().unwrap().trim().trim_start_matches('(').to_string();
             let right = sides.next().unwrap().trim().trim_end_matches(')').to_string();
-            nodes.push(Node {
+            nodebuilders.push(NodeBuilder {
                 from,
                 left,
                 right,
             });
         }
-        let current = nodes.iter().filter(|node| node.from.ends_with('A')).map(|node| node.from.clone()).collect();
+        let mut nodes = Vec::new();
+        for nodebuilder in nodebuilders.clone() {
+            let left = nodebuilders.iter().position(|n| n.from == nodebuilder.left).unwrap();
+            let right = nodebuilders.iter().position(|n| n.from == nodebuilder.right).unwrap();
+            nodes.push(Node {
+                from: nodebuilder.from,
+                left,
+                right,
+            });
+        }
+        let current_nodes: Vec<Node> = nodes.clone().into_iter().filter(|node| node.from.ends_with('A')).collect();
+        let current: Vec<usize> = current_nodes.iter().map(|node| nodes.iter().position(|n| n.from == node.from).unwrap()).collect();
         Self {
             nodes,
             current,
@@ -83,10 +101,9 @@ impl Network {
     fn walk(&mut self, steps: &mut Steps) {
         let step = steps.next();
         for current in &mut self.current {
-            let node = self.nodes.iter().find(|n| n.from == *current).unwrap();
             *current = match step {
-                'L' => node.left.clone(),
-                'R' => node.right.clone(),
+                'L' => self.nodes[*current].left,
+                'R' => self.nodes[*current].right,
                 _ => panic!("Invalid step: {}", step),
             };
         }
