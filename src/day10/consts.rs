73e48ca78d999537b448d05a2281ec16cb7b3726
eric valcik <eric.valcik@gmail.com>
2023-12-10 23:15:26 +0100

    day10 part 2

--- src/day10/consts.rs
+++ src/day10/consts.rs
@@ -6,11 +6,60 @@ SJLL7
 LJ.LJ";
 
 pub const EXAMPLE1: &str = "
--L|F7
+-7|F7
 7S-7|
 L|7||
 -L-J|
 L|-JF";
+
+pub const EXAMPLE3: &str = "\
+...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........";
+
+// right answer: 8 tiles inside
+pub const EXAMPLE4: &str = "
+.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...";
+
+// right answer: 4
+pub const EXAMPLE5: &str = "
+..........
+.S------7.
+.|F----7|.
+.||....||.
+.||....||.
+.|L-7F-J|.
+.|..||..|.
+.L--JL--J.
+..........";
+
+// right answer: 10
+pub const EXAMPLE6: &str = "
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L";
 
 pub const INPUT: &str = "
 F|--L7J.L7|7|-LF7-7--7F7--7F7J--F7J.7F|J7.F7|.-FJF|J.F7F--LFF.7.F7JFL-JFJ7F|.FL77-F7-J.F7-F7L-FF-F-L7F-|7.FL7-7F|7F7.F-F|-77FJ7FF-J777.|FFFL
--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -4,15 +4,15 @@ mod consts;
 
 pub fn find_furthest() -> usize {
     let mut map: Map = Map::new(consts::INPUT);
-    let maxY = map.map.len() - 1;
-    let maxX = map.map[0].len() - 1;
+    let max_y = map.map.len() - 1;
+    let max_x = map.map[0].len() - 1;
     let start = map.find_start().unwrap();
     let mut queue: VecDeque<(Coords, usize)> = VecDeque::new();
     queue.push_back((start, 0));
     while !queue.is_empty() {
         let (coords, distance) = queue.pop_front().unwrap();
         let mut tiles = &mut map.map;
-        if coords.x > maxX || coords.y > maxY {
+        if coords.x > max_x || coords.y > max_y {
             continue;
         }
         if tiles[coords.y][coords.x].distance.is_some() {
@@ -24,7 +24,7 @@ pub fn find_furthest() -> usize {
         for direction in &[Direction::Up, Direction::Down, Direction::Left, Direction::Right] {
             if tiles[coords.y][coords.x].is_connected(direction) {
                 if let Some(shifted_coords) = get_shifted_coords(&coords, direction) {
-                    if shifted_coords.x > maxX || shifted_coords.y > maxY {
+                    if shifted_coords.x > max_x || shifted_coords.y > max_y {
                         continue;
                     }
                     if tiles[shifted_coords.y][shifted_coords.x].is_connected(&get_opposite_direction(direction)) {
@@ -34,8 +34,25 @@ pub fn find_furthest() -> usize {
             }
         }
     }
-    map.print_distance();
-    map.find_highest_loop_val()
+    let ret = map.find_highest_loop_val();
+    map.print_loop();
+    let start_coords = map.extend_map();
+    if !map.flood(Coords { x: start_coords.x + 1, y: start_coords.y + 1 }, b'a') {
+        println!("a: {}", map.count_flooded_tiles(b'a'));
+    }
+    if !map.flood(Coords { x: start_coords.x - 1, y: start_coords.y + 1 }, b'b') {
+        println!("b: {}", map.count_flooded_tiles(b'b'));
+    }
+    if !map.flood(Coords { x: start_coords.x + 1, y: start_coords.y - 1 }, b'c') {
+        println!("c: {}", map.count_flooded_tiles(b'c'));
+    }
+    if !map.flood(Coords { x: start_coords.x - 1, y: start_coords.y - 1 }, b'd') {
+        println!("d: {}", map.count_flooded_tiles(b'd'));
+    }
+    // map.print_extended_map();
+    println!();
+    // map.print_flooded_small();
+    ret
 }
 
 #[derive(Clone)]
@@ -67,6 +84,7 @@ const fn get_shifted_coords(coords: &Coords, direction: &Direction) -> Option<Co
 
 struct Map {
     map: Vec<Vec<Tile>>,
+    extended_map: Option<Vec<Vec<u8>>>,
 }
 
 impl Map {
@@ -80,8 +98,40 @@ impl Map {
             map.push(row);
         }
         Self {
-            map
+            map,
+            extended_map: None,
         }
+    }
+
+    // returns location of b'S'
+    fn extend_map(&mut self) -> Coords {
+        let mut start_coords = Coords { x: 0, y: 0 };
+        let mut extended_map = Vec::new();
+        for (y, row) in self.map.iter().enumerate() {
+            let mut extended_row_up = vec![b'e'; row.len() * 3];
+            let mut extended_row_middle = vec![b'e'; row.len() * 3];
+            let mut extended_row_down = vec![b'e'; row.len() * 3];
+            for (x, col) in row.iter().enumerate() {
+                if col.char == b'S' {
+                    start_coords = Coords { x: x * 3 + 1, y: y * 3 + 1 };
+                }
+                let array = char_to_array(if col.part_of_loop { col.char } else { b'.' });
+                extended_row_up[x * 3] = array[0][0];
+                extended_row_up[x * 3 + 1] = array[0][1];
+                extended_row_up[x * 3 + 2] = array[0][2];
+                extended_row_middle[x * 3] = array[1][0];
+                extended_row_middle[x * 3 + 1] = array[1][1];
+                extended_row_middle[x * 3 + 2] = array[1][2];
+                extended_row_down[x * 3] = array[2][0];
+                extended_row_down[x * 3 + 1] = array[2][1];
+                extended_row_down[x * 3 + 2] = array[2][2];
+            }
+            extended_map.push(extended_row_up);
+            extended_map.push(extended_row_middle);
+            extended_map.push(extended_row_down);
+        }
+        self.extended_map = Some(extended_map);
+        start_coords
     }
 
     fn find_start(&self) -> Option<Coords> {
@@ -103,14 +153,38 @@ impl Map {
         self.map.get(coords.y)?.get(coords.x)
     }
 
-    fn find_highest_loop_val(&self) -> usize {
+    fn find_highest_loop_val(&mut self) -> usize {
+        let mut highest_coords: Option<Coords> = None;
         let mut highest = 0;
         for row in 0..self.map.len() {
             for col in 0..self.map[row].len() {
                 let tile = self.get_tile(&Coords { x: col, y: row }).unwrap();
                 if let Some(distance) = tile.distance {
                     if distance > highest && self.can_tile_be_highest(&Coords { x: col, y: row }) {
+                        highest_coords = Some(Coords { x: col, y: row });
                         highest = distance;
+                    }
+                }
+            }
+        }
+        let mut queue: VecDeque<Coords> = VecDeque::new();
+        queue.push_back(highest_coords.unwrap());
+        while !queue.is_empty() {
+            let coords = queue.pop_front().unwrap();
+            self.get_tile_mut(&coords).unwrap().part_of_loop = true;
+            if self.get_tile(&coords).unwrap().char == b'S' {
+                continue;
+            }
+            for direction in &[Direction::Up, Direction::Down, Direction::Left, Direction::Right] {
+                if !self.get_tile(&coords).unwrap().is_connected(direction) {
+                    continue;
+                }
+                if let Some(shifted_coords) = get_shifted_coords(&coords, direction) {
+                    if let Some(tile) = self.get_tile(&shifted_coords) {
+                        if tile.is_connected(&get_opposite_direction(direction))
+                            && !tile.part_of_loop {
+                            queue.push_back(shifted_coords);
+                        }
                     }
                 }
             }
@@ -140,7 +214,75 @@ impl Map {
     fn print_distance(&self) {
         for row in &self.map {
             for tile in row {
-                tile.distance.map_or_else(|| print!(".. "), |distance| print!("{:02} ", distance))
+                tile.distance.map_or_else(|| print!(".. "), |distance| print!("{distance:02} "));
+            }
+            println!();
+        }
+    }
+
+    fn print_loop(&self) {
+        for row in &self.map {
+            for tile in row {
+                if tile.part_of_loop {
+                    print!("X");
+                } else {
+                    print!(".");
+                }
+            }
+            println!();
+        }
+    }
+
+    fn print_extended_map(&self) {
+        for row in self.extended_map.as_ref().unwrap() {
+            for tile in row {
+                print!("{}", *tile as char);
+            }
+            println!();
+        }
+    }
+
+    fn flood(&mut self, coords: Coords, char: u8) -> bool {
+        let mut queue: VecDeque<Coords> = VecDeque::new();
+        let mut touched_wall = false;
+        queue.push_back(coords);
+        while !queue.is_empty() {
+            let coords = queue.pop_front().unwrap();
+            if coords.y >= self.extended_map.as_ref().unwrap().len() || coords.x >= self.extended_map.as_ref().unwrap()[coords.y].len() {
+                touched_wall = true;
+                continue;
+            }
+            if self.extended_map.as_ref().unwrap()[coords.y][coords.x] != b'.' {
+                continue;
+            }
+            self.extended_map.as_mut().unwrap()[coords.y][coords.x] = char;
+            for direction in &[Direction::Up, Direction::Down, Direction::Left, Direction::Right] {
+                get_shifted_coords(&coords, direction).map_or_else(|| {
+                    touched_wall = true;
+                }, |shifted_coords| {
+                    queue.push_back(shifted_coords);
+                });
+            }
+        }
+        touched_wall
+    }
+
+    fn count_flooded_tiles(&self, char: u8) -> usize {
+        let mut count: usize = 0;
+        for row in 0..self.map.len() {
+            for col in 0..self.map[row].len() {
+                if self.extended_map.as_ref().unwrap()[row * 3 + 1][col * 3 + 1] == char {
+                    count += 1;
+                }
+            }
+        }
+        count
+    }
+
+    fn print_flooded_small(&self) {
+        for row in 0..self.map.len() {
+            for col in 0..self.map[row].len() {
+                print!("{}", self.extended_map.as_ref().unwrap()[row * 3 + 1][col * 3 + 1] as char);
             }
             println!();
         }
@@ -150,6 +292,7 @@ impl Map {
 struct Tile {
     char: u8,
     distance: Option<usize>,
+    part_of_loop: bool,
 }
 
 enum Direction {
@@ -173,6 +316,7 @@ impl Tile {
         Self {
             char,
             distance: None,
+            part_of_loop: false,
         }
     }
 
@@ -190,5 +334,54 @@ impl Tile {
 
     fn is_pipe(&self) -> bool {
         [b'S', b'|', b'-', b'J', b'L', b'F', b'7'].contains(&self.char)
+    }
+    const fn is_empty(&self) -> bool {
+        self.char == b' '
+    }
+}
+
+fn char_to_array(char: u8) -> [[u8; 3]; 3] {
+    match char {
+        b'.' => [
+            [b'.', b'.', b'.'],
+            [b'.', b'.', b'.'],
+            [b'.', b'.', b'.']
+        ],
+        b'S' => [
+            [b'.', b'x', b'.'],
+            [b'x', b'x', b'x'],
+            [b'.', b'x', b'.']
+        ],
+        b'|' => [
+            [b'.', b'x', b'.'],
+            [b'.', b'x', b'.'],
+            [b'.', b'x', b'.']
+        ],
+        b'-' => [
+            [b'.', b'.', b'.'],
+            [b'x', b'x', b'x'],
+            [b'.', b'.', b'.']
+        ],
+        b'J' => [
+            [b'.', b'x', b'.'],
+            [b'x', b'x', b'.'],
+            [b'.', b'.', b'.']
+        ],
+        b'L' => [
+            [b'.', b'x', b'.'],
+            [b'.', b'x', b'x'],
+            [b'.', b'.', b'.']
+        ],
+        b'F' => [
+            [b'.', b'.', b'.'],
+            [b'.', b'x', b'x'],
+            [b'.', b'x', b'.']
+        ],
+        b'7' => [
+            [b'.', b'.', b'.'],
+            [b'x', b'x', b'.'],
+            [b'.', b'x', b'.']
+        ],
+        _ => panic!("Invalid char: {}", char)
     }
 }
