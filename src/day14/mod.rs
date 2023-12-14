9d80701eef4b0bff1a3578a4abd18f41a8582bd4
eric valcik <eric.valcik@gmail.com>
2023-12-14 11:57:22 +0100

    day14 part 2 without optimalizations

--- src/day14/mod.rs
+++ src/day14/mod.rs
@@ -1,8 +1,13 @@
 mod consts;
 
 pub fn calculate_load() -> usize {
-    let mut map = Map::new(consts::INPUT.trim());
-    map.roll_up();
+    let mut map = Map::new(consts::EXAMPLE1.trim());
+    for i in 0..1_000_000_000 {
+        if i % 100_000 == 0 {
+            println!("{i}");
+        }
+        map.roll_cycle();
+    }
     map.print_map();
     map.calc_load()
 }
@@ -25,16 +30,17 @@ impl Map {
         self.grid.len()
     }
 
-    pub fn roll_up(&mut self) {
+    pub fn roll_vertical(&mut self, up: bool) {
         for col in 0..self.width() {
             let mut can_roll_to: Option<usize> = None;
             for row in 0..self.height() {
+                let row = if up { row } else { self.height() - row - 1 };
                 if self.grid[row][col] == 'O' {
                     // try to roll it up
                     if let Some(roll_to) = can_roll_to {
                         self.grid[roll_to][col] = 'O';
                         self.grid[row][col] = '.';
-                        can_roll_to = Some(roll_to + 1);
+                        can_roll_to = Some(if up {roll_to + 1} else {roll_to - 1});
                     }
                 } else if self.grid[row][col] == '#' {
                     can_roll_to = None;
@@ -43,6 +49,34 @@ impl Map {
                 }
             }
         }
+    }
+
+    pub fn roll_horizontal(&mut self, left: bool) {
+        for row in 0..self.height() {
+            let mut can_roll_to: Option<usize> = None;
+            for col in 0..self.width() {
+                let col = if left { col } else { self.width() - col - 1 };
+                if self.grid[row][col] == 'O' {
+                    // try to roll it up
+                    if let Some(roll_to) = can_roll_to {
+                        self.grid[row][roll_to] = 'O';
+                        self.grid[row][col] = '.';
+                        can_roll_to = Some(if left {roll_to + 1} else {roll_to - 1});
+                    }
+                } else if self.grid[row][col] == '#' {
+                    can_roll_to = None;
+                } else if self.grid[row][col] == '.' && can_roll_to.is_none() {
+                    can_roll_to = Some(col);
+                }
+            }
+        }
+    }
+
+    pub fn roll_cycle(&mut self) {
+        self.roll_vertical(true);
+        self.roll_horizontal(true);
+        self.roll_vertical(false);
+        self.roll_horizontal(false);
     }
 
     pub fn print_map(&self) {
